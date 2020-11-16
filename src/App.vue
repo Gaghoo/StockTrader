32cf57e3ac4e7fd94c6fe52f7d8bfba3452c4986
Gaghoo <helomzive@gmail.com>
2020-11-16 13:45:01 -0300

    criaçao do Header e separação dos components e views

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-    
+    <v-app>
+        <Header/>
+        <v-main>
+            <router-view/>
+        </v-main>
+    </v-app>
 </template>
 
 
-
 <script>
+import Header from './components/Header'
 export default {
-    
+    components:{
+        Header
+    }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,63 @@
 <template>
-    <h1>Header</h1>
+    <nav>
+        <v-app-bar app>
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title>
+                <span class="font-weight-thin">Stock </span>
+                <span>Trade</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn text>
+                    Finalizar Dia
+                </v-btn>
+                <v-menu offset-y>
+                    <template v-slot:activator="{ on, attrs }">
+                        <v-btn v-bind="attrs" v-on="on" text>
+                        Salvar & Carregar
+                        </v-btn>
+                    </template>
+                    <v-list>
+                        <v-list-item
+                        v-for="(item, index) in items"
+                        :key="index"
+                        >
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                        </v-list-item>
+                    </v-list>
+                    </v-menu>
+            </v-toolbar-items>
+        </v-app-bar>
+        <v-navigation-drawer app v-model="drawer">
+            <v-list shaped>
+                <v-list-item-group v-model="selectedItem" color="info">
+                    <v-list-item v-for="(link, i) in links" :key="i" :to="link.route">
+                        <v-list-item-title v-text="link.text"></v-list-item-title>
+                        <v-list-item-icon>
+                            <v-icon v-text="link.icon"></v-icon>
+                        </v-list-item-icon>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+    </nav>
 </template>
 
 
 <script>
 export default {
-    
+    data: () => ({
+        drawer: false,
+        selectedItem: 0,
+        links:[
+            {icon: 'mdi-home-outline', text: 'Home Page', route: '/'},
+            {icon: 'mdi-bank-transfer-out', text: 'Portfólio', route: '/portfolio'},
+            {icon: 'mdi-bank-transfer-in', text: 'Ações', route: '/stocks'}
+        ],
+        items: [
+        { title: 'Salvar Dados' },
+        { title: 'Carregar Dados' },
+      ]
+    })
 }
 </script>
