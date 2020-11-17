cbfd074895ea51769aea9de3050fc8300b619107
Gaghoo <helomzive@gmail.com>
2020-11-17 16:37:44 -0300

    implementação e configuração previa do vuex

---
+++ src/data/stocks.js
@@ -0,0 +1,8 @@
+export default [
+    {id: 1, name: 'Atacadão', price: 27},
+    {id: 2, name: 'Inspire Academia', price: 58},
+    {id: 3, name: 'Google', price: 207},
+    {id: 4, name: 'BMW', price: 512},
+    {id: 5, name: 'Mercado Livre', price: 270},
+    {id: 6, name: 'Maia Construção', price: 36},
+]
--- src/views/Stocks.vue
+++ src/views/Stocks.vue
@@ -9,17 +9,15 @@
 import StockBuy from '../components/StockBuy'
 export default {
     data:() => ({
-        stocks: [
-            {id: 1, name: 'Atacadão', price: 27},
-            {id: 2, name: 'Inspire Academia', price: 58},
-            {id: 3, name: 'Google', price: 207},
-            {id: 4, name: 'BMW', price: 512},
-            {id: 5, name: 'Mercado Livre', price: 270},
-            {id: 6, name: 'Maia Construção', price: 36},
-        ]
+        
     }),
     components:{
         StockBuy
+    },
+    computed:{
+        stocks(){
+            return this.$store.getters.stocks
+        }
     }
 }
 </script>
