15699854747c1c2b5ccb21157a2c2cd0da69ee56
Gaghoo <helomzive@gmail.com>
2020-11-17 14:10:52 -0300

    criação e implementação do componente responsavel pela compra de ações, feita tambem a validaçao para nao permitir compra de valores negativos ou nao inteiros

--- src/components/StockBuy.vue
+++ src/components/StockBuy.vue
@@ -1,10 +1,41 @@
 <template>
-    <h1>Ação para compra</h1>
+    <v-col class="pr-3 pb-5" lg="3" sm="6"  md="4">
+        <v-card shaped>
+            <v-card-title class="green draken-4 white--text headline">
+                <strong>{{stock.name}} </strong><small class="caption">{{stock.price}}</small>
+            </v-card-title>
+            <v-card-text class="">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </v-card-text>
+            <v-card-actions>
+                <v-text-field label="Comprar ação" type="number" color="green draken-4" v-model.number="quantity"/>
+                <v-btn rounded class="mx-2" text color="green draken-4" small 
+                    @click="buyStock" :disabled="quantity <=0 || !Number.isInteger(quantity)">
+                    <v-icon>mdi-login-variant</v-icon>
+                    <span>comprar</span>
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-col>
 </template>
 
 
 <script>
 export default {
-    
+    props: ['stock'],
+    data:()=>({
+        quantity: 0
+    }),
+    methods:{
+        buyStock () {
+            const order = {
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            }
+            console.log(order)
+            this.quantity = 0
+        }
+    }
 }
 </script>
--- src/views/Stocks.vue
+++ src/views/Stocks.vue
@@ -1,10 +1,25 @@
 <template>
-    <h1>Mercado de ações</h1>
+    <v-row>
+        <StockBuy v-for="(stock, index) in stocks" :key="index" :stock="stock"/>
+    </v-row>
 </template>
 
 
 <script>
+import StockBuy from '../components/StockBuy'
 export default {
-    
+    data:() => ({
+        stocks: [
+            {id: 1, name: 'Atacadão', price: 27},
+            {id: 2, name: 'Inspire Academia', price: 58},
+            {id: 3, name: 'Google', price: 207},
+            {id: 4, name: 'BMW', price: 512},
+            {id: 5, name: 'Mercado Livre', price: 270},
+            {id: 6, name: 'Maia Construção', price: 36},
+        ]
+    }),
+    components:{
+        StockBuy
+    }
 }
 </script>
